e6df323d5b07fbcb30e5dc4f9f83ffb3ca7514c0
mafsa7 <mafsa7@users.noreply.github.com>
2020-11-17 22:29:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 Vue.component('jumbo', {
-    template: '<div class="jumbotron text-center"> <h1>My First Bootstrap Page</h1><p>Resize this responsive page to see the effect!</p></div>'
+    template: '<div class="jumbotron text-center"> <h1>My Web Page</h1><p>Resize this responsive page to see the effect!</p></div>'
 });
 
 const app = new Vue({
